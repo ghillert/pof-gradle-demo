0d84aa910ba49b0b815dd0f0d2369d284e26405a
Gunnar Hillert <gunnar@hillert.com>
2023-07-13 23:06:15 -1000

    Add POF Gradle Demo

---
+++ src/main/java/com/hillert/coherence/pof/PofGradleApplication.java
@@ -0,0 +1,14 @@
+package com.hillert.coherence.pof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PofGradleApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!!");
+		SpringApplication.run(PofGradleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hillert/coherence/pof/PofGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hillert.coherence.pof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PofGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
